336b3fb926ceb1ac9f7a6ef35f7a6e7a55a1a5a5
Michael Cardenas <michramcar1@gmail.com>
2025-03-21 17:53:56 -0400

    Fixed images in Comments

--- src/components/Comments/Comments.config.ts
+++ src/components/Comments/Comments.config.ts
@@ -2,7 +2,7 @@ export const COMMENTS_CLIENTS_CANDIDATES_DATA: CardComment[] = [
   {
     comment: "The follow-up and support you provide to candidates throughout the process really stand out. Kudos to the entire Purrfect Hire team for the service delivered, and to my recruiter for being always caring and helpful!.",
     name: "Mackley Carmona",
-    img: "https://media.licdn.com/dms/image/v2/D4E03AQHuUIjTqGl4gg/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1730821138618?e=1736985600&v=beta&t=bZHlox5Z7fAYB5vl9pwo17iIlrjyoXEpa0Uy3lfHkm4",
+    img: "/comments-img/mackleyCarmona.jpeg",
     country: "Colombia",
     role: "Founder Engineer",
     company: "PONTE 🇺🇸",
@@ -11,7 +11,7 @@ export const COMMENTS_CLIENTS_CANDIDATES_DATA: CardComment[] = [
   {
     comment: "Purrfecthire accompanied me throughout my selection process, notifying me and reminding me of interview dates. I appreciate that they helped me find a position where I am happy and that meets my expectations.",
     name: "Sebasthian Saldana",
-    img: "https://media.licdn.com/dms/image/v2/D4E03AQGvxD2tm7oo-Q/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1689990166577?e=1731542400&v=beta&t=AF9EnKPDBV3HJivvN6mCe4vB8MRC6U7D2GLpuuGxZS0",
+    img: "/comments-img/sebasthianSaldana.jpeg",
     country: "México",
     role: "Accountant",
     company: "Konta 🇲🇽",
@@ -20,7 +20,7 @@ export const COMMENTS_CLIENTS_CANDIDATES_DATA: CardComment[] = [
   {
     comment: "I loved the process with Purrfect Hire! A process that understands the culture of companies and the personality of candidates allows cultural matches to be made from moment zero. In addition to that, candidates feel supported by a team that is attentive at every moment of the process.",
     name: "Andrés F. Hortúa",
-    img: "https://media.licdn.com/dms/image/v2/D4E03AQEG1UhURKoOCg/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1710771595672?e=1731542400&v=beta&t=PTMp4tSEoJ9mhXHXmzuzbEEXPPX5ZbdflmxJajzQTks",
+    img: "/comments-img/andresFHortua.jpeg",
     country: "Colombia",
     role: "Head of Growth",
     company: "Bolsiyo 🇨🇴",
@@ -29,7 +29,7 @@ export const COMMENTS_CLIENTS_CANDIDATES_DATA: CardComment[] = [
   {
     comment: "I'm completely grateful with you. This has been an superb experience. I've never been in such a supportive process.",
     name: "Diana Arteaga",
-    img: "https://media.licdn.com/dms/image/v2/C5603AQHLi5iBU_pkmw/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1595694262517?e=1731542400&v=beta&t=-PEBfq6HQk_C5O4KSjj2Yk8cpa6FsUpBirUWG3d2kRQ",
+    img: "/comments-img/dianaArteaga.jpeg",
     country: "Colombia",
     role: "Customer Service Lead",
     company: "Konta 🇲🇽",
@@ -41,7 +41,7 @@ export const COMMENTS_CLIENTS_CANDIDATES_DATA: CardComment[] = [
   {
     comment: "Thanks to the experience and industry knowledge that Purrfect Hire has, they got me the best candidates out there so the final selection was somehow easy.",
     name: "Camilo Rodríguez ",
-    img: "https://media.licdn.com/dms/image/v2/D4D03AQH-Co5Tcn3aFw/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1703185394089?e=1731542400&v=beta&t=MDecYz3HWHpirP92zFfqa6mdJAli8205cSNEnQ6Rm3w",
+    img: "/comments-img/camiloRodriguez.jpeg",
     country: "Argentina",
     role: "Sales Director",
     company: "Ed-Machina 🇦🇷",
@@ -50,7 +50,7 @@ export const COMMENTS_CLIENTS_CANDIDATES_DATA: CardComment[] = [
   {
     comment: "The people we've hired through Purrfect Hire not only continue working and adding value, but they've also been promoted to roles of greater responsibility due to how well they've adapted to our way of working and our culture.",
     name: "Micaela Crosta",
-    img: "https://media.licdn.com/dms/image/v2/D4D03AQG66UJqROQjbg/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1727676584998?e=1736985600&v=beta&t=OTxUeHWGaZYcUnKrclrvGbnHk30wx-7p8mRmyRt-iAQ",
+    img: "/comments-img/micaelaCrosta.jpeg",
     country: "EEUU",
     role: "Head of Customer Operations",
     company: "OutDo 🇺🇸",
@@ -59,7 +59,7 @@ export const COMMENTS_CLIENTS_CANDIDATES_DATA: CardComment[] = [
   {
     comment: "Purrfect Hire is a great talent search partner. They adapted to our processes and to each specific role to help us bring the best talent to our company.",
     name: "Fabián Torres",
-    img: "/fabianTorres.jpg",
+    img: "/comments-img/fabianTorres.jpeg",
     country: "Colombia",
     role: "CEO",
     company: "Trébol 🇨🇴",
