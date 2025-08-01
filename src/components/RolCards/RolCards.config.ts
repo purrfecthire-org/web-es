8174e31ae07efd2beb30ea7b4c7f2e7a03f80909
RichardAyalaPH <richard@purrfecthire.com>
2025-08-01 14:47:42 -0500

    Update RolCards.config.ts

--- src/components/RolCards/RolCards.config.ts
+++ src/components/RolCards/RolCards.config.ts
@@ -5,7 +5,8 @@ export const jobPosition: JobPosition[] = [
     modality: "Hybrid: Bogotá",
     salary: "120.000 - 150.000 USD/yr",
     skills: ["Technical Strategy", "Team Leadership", "System Architecture"],
-    link: "https://www.linkedin.com/company/purrfect-hire/"
+    link: "https://yt3.googleusercontent.com/EPZEFd_lEbHQcAAesEsvRKoGneunmoGqu2vptExT4VFFqSucT-gjKeTCUMLeE6Jxtc2ApUvjLSw=s900-c-k-c0x00ffffff-no-rj",
+    image_url: "/logo.png"
   },
   {
     title: "AI Engineer",
@@ -13,7 +14,8 @@ export const jobPosition: JobPosition[] = [
     modality: "Hybrid: San Francisco",
     salary: "150.000 - 190.000 USD/yr",
     skills: ["AI", "Machine Learning", "Data Science"],
-    link: "https://www.linkedin.com/company/purrfect-hire/"
+    link: "https://www.linkedin.com/company/purrfect-hire/",
+    image_url: "/logo.png"
   },
   {
     title: "AI Developer Junior",
@@ -21,7 +23,8 @@ export const jobPosition: JobPosition[] = [
     modality: "Remote",
     salary: "100.000 - 140.000 USD/yr",
     skills: ["AI", "Machine Learning", "Data Science"],
-    link: "https://www.linkedin.com/company/purrfect-hire/"
+    link: "https://www.linkedin.com/company/purrfect-hire/",
+    image_url: "/logo.png"
   },
   {
     title: "CISO",
@@ -29,7 +32,8 @@ export const jobPosition: JobPosition[] = [
     modality: "On site",
     salary: "60.000 - 80.000 USD/yr",
     skills: ["Cybersecurity", "Risk Management", "Security Architecture"],
-    link: "https://www.linkedin.com/company/purrfect-hire/"
+    link: "https://www.linkedin.com/company/purrfect-hire/",
+    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUECw31cj6_EeUy1ecIyKlKQ2O-J-TEu_EIQ"
   },
   {
     title: "Business Developer",
@@ -37,7 +41,8 @@ export const jobPosition: JobPosition[] = [
     modality: "On site",
     salary: "30.000 - 36.000 USD/yr",
     skills: ["Business Strategy", "Partnership Development", "Market Analysis"],
-    link: "https://www.linkedin.com/posts/kathforero_urgent-hiring-for-one-of-yc-backed-companies-activity-7355683884388229120-bk22?utm_source=share&utm_medium=member_desktop&rcm=ACoAADGO1qcBAXHsrRgwTOjiZHAvC9XPiA5Kqok"
+    link: "https://www.linkedin.com/posts/kathforero_urgent-hiring-for-one-of-yc-backed-companies-activity-7355683884388229120-bk22?utm_source=share&utm_medium=member_desktop&rcm=ACoAADGO1qcBAXHsrRgwTOjiZHAvC9XPiA5Kqok",
+    image_url: "https://play-lh.googleusercontent.com/rfuiETJBxP_zh6C5FnEw_51cpPqzygsH0bfEgkrEzZyM49nsArS7Juywy_deJMfQ6Hc"
   },
   {
     title: "Fullstack Developer",
@@ -45,7 +50,8 @@ export const jobPosition: JobPosition[] = [
     modality: "On site",
     salary: "72.000 - 96.000 USD/yr",
     skills: ["React", "Node.js", "TypeScript"],
-    link: "https://www.linkedin.com/posts/kathforero_urgent-hiring-for-one-of-yc-backed-companies-activity-7355683884388229120-bk22?utm_source=share&utm_medium=member_desktop&rcm=ACoAADGO1qcBAXHsrRgwTOjiZHAvC9XPiA5Kqok"
+    link: "https://www.linkedin.com/posts/kathforero_urgent-hiring-for-one-of-yc-backed-companies-activity-7355683884388229120-bk22?utm_source=share&utm_medium=member_desktop&rcm=ACoAADGO1qcBAXHsrRgwTOjiZHAvC9XPiA5Kqok",
+    image_url: "https://cdn.imweb.me/upload/S202308237a37bd840a716/1bfb42b749afc.png"
   },
   {
     title: "Backend Engineer",
@@ -53,7 +59,8 @@ export const jobPosition: JobPosition[] = [
     modality: "Remote",
     salary: "24.000 - 27.600 USD/yr",
     skills: ["Python", "API Development", "Cloud Computing"],
-    link: "https://www.linkedin.com/posts/daniela-d%C3%ADaz-010b9b243_buscas-un-trabajo-remoto-retador-y-con-activity-7355987705136095232-qyxK/?utm_source=share&utm_medium=member_ios&rcm=ACoAADyCl0wBofVz-JJP3zIKlIgIvlrcla17P7E"
+    link: "https://www.linkedin.com/posts/daniela-d%C3%ADaz-010b9b243_buscas-un-trabajo-remoto-retador-y-con-activity-7355987705136095232-qyxK/?utm_source=share&utm_medium=member_ios&rcm=ACoAADyCl0wBofVz-JJP3zIKlIgIvlrcla17P7E",
+    image_url: "https://play-lh.googleusercontent.com/rfuiETJBxP_zh6C5FnEw_51cpPqzygsH0bfEgkrEzZyM49nsArS7Juywy_deJMfQ6Hc"
   },
   {
     title: "Sr Full Stack Engineer",
@@ -61,7 +68,8 @@ export const jobPosition: JobPosition[] = [
     modality: "Remote",
     salary: "60.000 - 78.000 USD/yr",
     skills: ["React", "Node.js", "System Architecture"],
-    link: "https://www.linkedin.com/posts/daniela-d%C3%ADaz-010b9b243_buscas-un-trabajo-remoto-retador-y-con-activity-7355987705136095232-qyxK/?utm_source=share&utm_medium=member_ios&rcm=ACoAADyCl0wBofVz-JJP3zIKlIgIvlrcla17P7E"
+    link: "https://www.linkedin.com/posts/daniela-d%C3%ADaz-010b9b243_buscas-un-trabajo-remoto-retador-y-con-activity-7355987705136095232-qyxK/?utm_source=share&utm_medium=member_ios&rcm=ACoAADyCl0wBofVz-JJP3zIKlIgIvlrcla17P7E",
+    image_url: "https://play-lh.googleusercontent.com/UzZTMEVbe0ujhHarV2tIPolN_mMv7ZXi200MHj3rirYoZByrLLlhy4iSgLgHUvQLGahc"
   },
   {
     title: "Data Analyst",
@@ -69,7 +77,8 @@ export const jobPosition: JobPosition[] = [
     modality: "Remote",
     salary: "24.000 - 30.000 USD/yr",
     skills: ["SQL", "Python", "Data Visualization"],
-    link: "https://www.linkedin.com/posts/daniela-d%C3%ADaz-010b9b243_buscas-un-trabajo-remoto-retador-y-con-activity-7355987705136095232-qyxK/?utm_source=share&utm_medium=member_ios&rcm=ACoAADyCl0wBofVz-JJP3zIKlIgIvlrcla17P7E"
+    link: "https://www.linkedin.com/posts/daniela-d%C3%ADaz-010b9b243_buscas-un-trabajo-remoto-retador-y-con-activity-7355987705136095232-qyxK/?utm_source=share&utm_medium=member_ios&rcm=ACoAADyCl0wBofVz-JJP3zIKlIgIvlrcla17P7E",
+    image_url: "https://play-lh.googleusercontent.com/rfuiETJBxP_zh6C5FnEw_51cpPqzygsH0bfEgkrEzZyM49nsArS7Juywy_deJMfQ6Hc"
   },
   {
     title: "SDR",
@@ -77,22 +86,25 @@ export const jobPosition: JobPosition[] = [
     modality: "On site",
     salary: "24.000 - 36.000 USD/yr",
     skills: ["Sales Development", "Lead Generation", "CRM Management"],
-    link: "https://www.linkedin.com/posts/daniela-d%C3%ADaz-010b9b243_buscas-un-trabajo-remoto-retador-y-con-activity-7355987705136095232-qyxK/?utm_source=share&utm_medium=member_ios&rcm=ACoAADyCl0wBofVz-JJP3zIKlIgIvlrcla17P7E"
+    link: "https://www.linkedin.com/posts/daniela-d%C3%ADaz-010b9b243_buscas-un-trabajo-remoto-retador-y-con-activity-7355987705136095232-qyxK/?utm_source=share&utm_medium=member_ios&rcm=ACoAADyCl0wBofVz-JJP3zIKlIgIvlrcla17P7E",
+    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUECw31cj6_EeUy1ecIyKlKQ2O-J-TEu_EIQ&s"
   },
   {
     title: "Sr. Backend Engineer",
     country: "Colombia",
     modality: "On site",
-    salary: "60.000 - 90.000 USD/yr",
+    salary: "60.000 - 90.000 USD/yr ",
     skills: ["System Architecture", "Microservices", "Team Leadership"],
-    link: "https://www.linkedin.com/company/purrfect-hire/"
+    link: "https://www.linkedin.com/company/purrfect-hire/",
+    image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUECw31cj6_EeUy1ecIyKlKQ2O-J-TEu_EIQ&s"
   },
   {
     title: "React Native Developer",
     country: "Latam",
     modality: "Remote",
-    salary: "24.000 - 36.000 USD/yr",
+    salary: "24.000 - 36.000 USD",
     skills: ["React Native", "Mobile Development", "JavaScript", "Redux", "API Integration"],
-    link: "https://www.linkedin.com/company/purrfect-hire/"
+    link: "https://www.linkedin.com/company/purrfect-hire/",
+    image_url: "https://play-lh.googleusercontent.com/rfuiETJBxP_zh6C5FnEw_51cpPqzygsH0bfEgkrEzZyM49nsArS7Juywy_deJMfQ6Hc"
   }
 ];
